78554ccc9481cd665de3546d408398be9b966735
topmasum <149712797+topmasum@users.noreply.github.com>
2024-03-22 20:48:24 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+document.addEventListener('DOMContentLoaded', function() {
+    // Trigger typing animation
+    const typingAnimation = document.getElementById('typing-animation');
+    typingAnimation.style.animationPlayState = 'running';
+});
+window.onload = function() {
+    window.location.href = '#home';
+};
